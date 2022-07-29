2333d75473a7040c75034302ee199070791ac97d
Nghi1100 <vovannghi20@gmail.com>
2022-07-29 18:04:06 +0700

    update-source

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
@@ -13,24 +12,6 @@ const Header = () => {
             height={42.18}
             alt="icon"
           />
-        </div>
-        <div className="ml-auto flex text-white">
-          <Link href={'/san-pham-phela'}></Link>
-          <Link href={'/san-pham-phela'}></Link>
-          <Link href={'/san-pham-phela'}></Link>
-          <Link href={'/san-pham-phela'}></Link>
-          <Link href={'/san-pham-phela'}></Link>
-          <Link href={'/san-pham-phela'}></Link>
-          <div>SẢN PHẨM</div>
-          <div className="relative">
-            <p>CÂU CHUYỆN THƯƠNG HIỆU</p>
-            <div className="absolute"></div>
-          </div>
-          <div>TIN TỨC</div>
-          <div>CỬA HÀNG</div>
-          <div>TUYỂN DỤNG</div>
-          <div>THẺ THÀNH VIÊN</div>
-          <div>LIÊN HỆ</div>
         </div>
       </div>
     </div>
--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,8 +1,5 @@
 import type { ReactNode } from 'react';
 
-import { Footer } from '@/components/Footer';
-import Header from '@/components/Header';
-
 type IMainProps = {
   meta: ReactNode;
   children: ReactNode;
@@ -12,10 +9,7 @@ const Main = (props: IMainProps) => (
   <div>
     <div className="w-full antialiased">
       {props.meta}
-
-      <Header />
       <div>{props.children}</div>
-      <Footer />
     </div>
   </div>
 );
